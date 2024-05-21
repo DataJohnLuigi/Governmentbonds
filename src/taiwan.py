e6f4a386794d02a41b55ff27ec8c1d416d432cbe
John Luigi Calumot <johnluigicalumot@Johns-MacBook-Air.local>
2024-05-21 19:01:24 +0800

    [CC]Cleaned Taiwan script

--- src/taiwan.py
+++ src/taiwan.py
@@ -36,15 +36,6 @@ def get_args():
         help="Output directory"
     )
 
-    arguments.add_argument(
-        '-env',
-        '--environ',
-        type=str,
-        required=True,
-        help=(
-            'Environment either dev or prod'
-        )
-    )
 
 
     return arguments.parse_args()
@@ -65,22 +56,7 @@ def generate_date_range(sdate, edate):
     return date_range
 
 
-def main(date_range, output, environ):  # Pass output directory as an argument
-
-    # if environ == 'dev':
-    # # defining proxies
-    #     proxies = {
-    #         "http": "http://10.167.19.17:3128",
-    #         "https": "http://10.167.19.17:3128"
-    #     }
-    # elif environ == 'prod':
-    #     # Proxy for prod
-    #      proxies = {
-    #          "http":"http://10.167.19.233:3128",
-    #          "https":"http://10.167.19.233:3128"
-    # 	}
-    # else:
-    #     print("Invalid argument please specify whether prod or dev")
+def main(date_range, output):  # Pass output directory as an argument
     
     for d in date_range:
         main_directory = f'{output}'
@@ -142,28 +118,6 @@ def main(date_range, output, environ):  # Pass output directory as an argument
         # Save the DataFrame to CSV file
         xls_curve.to_csv(f'{output_directory}/{d}.government_bonds_tw_yield_curve.csv.gz', index=False)
         
-        # # for Zero coupon yield curve
-
-        # xls_zero = xls_zero.iloc[:-footer_rows]  # Drop the row with the note
-        # xls_zero = xls_zero.drop(['Unnamed: 2'], axis=1)  # Drop irrelevant columns
-        # xls_zero = xls_zero.T.reset_index()
-        # xls_zero.columns = xls_zero.iloc[0]
-        # xls_zero = xls_zero.drop(0)
-        # xls_zero.columns.name = None
-        # xls_zero = xls_zero.drop(['期間(年)', 'Tenor (yr)', '1.期間1m、3 m及6m，表示1個月、3個月及6個月1m, 3m and 6m indicate1 month, 3 months and 6 months .',
-        #                           '2.零息殖利率曲線配適模型詳請參見本中心網站之相關文件下載', '說明 Remark：'], axis=1)
-        # xls_zero = xls_zero.iloc[:, :-2]
-        # xls_zero.reset_index(drop=True, inplace=True)
-        # xls_zero.rename(columns={'模型Model': 'curve_type'}, inplace=True)
-        # xls_zero['curve_type'] = xls_zero['curve_type'].replace('Cubic B-Spline', 'cubic_b-spline_spot_rate')
-        # xls_zero['curve_type'] = xls_zero['curve_type'].replace('Svensson', 'svensson_spot_rate')
-        # xls_zero.columns = xls_zero.columns.astype(str)
-
-        # # appending y on year columns
-        # for i in range(4, 63):
-        #     xls_zero.columns.values[i] += 'y'
-
-        # xls_zero.to_csv(f'{main_directory}/{d}.yield_curve_tw.csv.gz', index=False)
 
         # Deleting raw file
         if os.path.exists(filename):
@@ -177,6 +131,5 @@ if __name__ == '__main__':
     START_DATE = args.sdate
     END_DATE = args.edate
     OUTPUT_PATH = args.output
-    ENVIRON = args.environ
-    date_range = generate_date_range(START_DATE, END_DATE)  # Save the date range
-    main(date_range, OUTPUT_PATH,ENVIRON)  # Pass date range and output path to main function
+    date_range = generate_date_range(START_DATE, END_DATE)
+    main(date_range, OUTPUT_PATH) 
